ed1bea3ec82aa41769001691d3f6cf639e7e380f
gabs.renan <xiubanido@gmail.com>
2023-01-12 13:25:49 -0300

    menssagem de mal funcionamento do servidor

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,13 +2,14 @@ import { Product } from './product.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar'
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import {map, catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  navigate(arg0: string[]) {
+  navigate(_arg0: string[]) {
     throw new Error('Method not implemented.');
   }
 
@@ -18,18 +19,30 @@ export class ProductService {
 
    }
 
-  showMensage(msg: string): void {
+  showMensage(msg: string, isError: boolean = false): void {
     // o primeiro arg mostar a mensagem que vai aparecer, o segundo já é uma action,
     // o terceiro são algumas ações dele
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      verticalPosition: "top",
+      panelClass: isError ? ['.msg-error'] : ['.msg-success']
+    });
+    
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e: any): Observable<any> {
+    console.log(e)
+
+    this.showMensage('Ocorreu um erro!', true);
+    return EMPTY;
   }
 
   read(): Observable<Product[]> {
